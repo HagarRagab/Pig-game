4b0a8c444d30930d4e030c9523d1814c473b96ee
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2024-09-02 19:08:37 +0300

    finish game

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Pink Dice game
+
+## Game preview
+
+![alt text](<design/Screenshot 2024-09-02 185513.png>)
+
+## Game's idea
+
+Pink Dice is a simple game that demonstrates DOM manipulation and is designed for two players. Each player takes turns rolling the dice multiple times and can choose to press the hold button at any point to keep their score. However, if a player rolls a 1, they lose their current score, and it becomes the other player's turn. The first player will obtain 100 points wins the game.
+
+## Flowchart
+
+![alt text](<design/pink dice game flowchart.drawio.png>)
+
+## Objectives
+
+Improve DOM understanding and writing clean code.
--- script.js
+++ script.js
@@ -1,1 +1,102 @@
 'use strict';
+const player1Ele = document.querySelector('.player1');
+const player2Ele = document.querySelector('.player2');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const diceImg = document.querySelector('.dice');
+
+let playerTotals, activePlayer, currentScore, playing;
+
+//! Init function
+const init = function () {
+  playerTotals = {
+    player1: 0,
+    player2: 0,
+  };
+  activePlayer = 'player1';
+  currentScore = 0;
+  playing = true;
+
+  player1Ele.classList.remove('player--winner');
+  player2Ele.classList.remove('player--winner');
+  diceImg.classList.remove('hidden');
+  if (!document.querySelector('.player1').classList.contains('player--active'))
+    activatePlayer();
+  diplayTotalScore('player1');
+  diplayTotalScore('player2');
+};
+
+init();
+
+//! Switch player function
+const switchPlayer = function () {
+  activePlayer = activePlayer === 'player1' ? 'player2' : 'player1';
+  activatePlayer();
+};
+
+//! Activation function
+const activatePlayer = function () {
+  player1Ele.classList.toggle('player--active');
+  player2Ele.classList.toggle('player--active');
+};
+
+//! Display current score function
+const diplayCurrentScore = function () {
+  document.getElementById(`${activePlayer}--current`).textContent =
+    currentScore;
+};
+
+//! Display total score function
+function diplayTotalScore(player) {
+  document.getElementById(`${player}--score`).textContent =
+    playerTotals[player];
+}
+
+// User clicks Roll Dice button
+btnRoll.addEventListener('click', () => {
+  if (playing) {
+    // 1- Generate random number from 1 to 6
+    const randomDiceNum = Math.trunc(Math.random() * 6) + 1;
+    // 2- Display dice image
+    diceImg.src = `/images/dice-${randomDiceNum}.png`;
+    diceImg.alt = `Playing dice ${randomDiceNum}`;
+    // 3- Check if random number is 1
+    if (randomDiceNum === 1) {
+      currentScore = 0;
+      diplayCurrentScore();
+      switchPlayer();
+    } else {
+      currentScore += randomDiceNum;
+      diplayCurrentScore();
+    }
+  }
+});
+
+// User clicks Hold button
+btnHold.addEventListener('click', () => {
+  if (playing) {
+    // 1- Update total score
+    playerTotals[activePlayer] += currentScore;
+    // 2- Reset current score
+    currentScore = 0;
+    // 3- Display current score
+    diplayCurrentScore();
+    // 4- Display total score
+    document.getElementById(`${activePlayer}--score`).textContent =
+      playerTotals[activePlayer];
+    // 5- Check for the winner
+    if (playerTotals[activePlayer] >= 100) {
+      document
+        .querySelector(`.${activePlayer}`)
+        .classList.add('player--winner');
+      diceImg.classList.add('hidden');
+      playing = false;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// User clicks new game button
+btnNew.addEventListener('click', init);
